42f44f6abe8c8733ceb932ae32908db1e06ebfff
Jacob <jakubbawor@protonmail.com>
2026-02-07 15:47:00 +0100

    Działające wykrywanie streamów

--- Snapshoter.py
+++ Snapshoter.py
@@ -5,23 +5,45 @@ import os
 import time
 
 
+port_rtsp = 8554
+nazwy_rtsp = []
+streamy_rtsp = []
+
+
+
 # Ustalennie adresów wszystkich streamów rtsp
-api_streamy = requests.get('http://localhost:9997/v3/paths/list')
-print(api_streamy.json())
 
-api_konfiguracja = requests.get('http://localhost:9997/v3/config/global/get')
-print(api_konfiguracja.json())
+api_konfiguracja = requests.get('http://localhost:9997/v3/config/global/get').json()
+port_rtsp = api_konfiguracja["rtspAddress"]
 
-port_rtsp = 0
-streamy_rtsp = ['rtsp://localhost:8554/a','']
+
+api_streamy = requests.get('http://localhost:9997/v3/paths/list').json()
+
+
+for i in api_streamy['items']:
+    if i["source"]['type'] == 'rtspSession':
+        nazwy_rtsp.append(i['name'])
+
+print(nazwy_rtsp)
+
+
+for i in nazwy_rtsp:
+    streamy_rtsp.append(f'rtsp://localhost{port_rtsp}/'+i)
+
+print(streamy_rtsp)
 
 
 # Ustalenie głównej ścieżki zapisu
+
 snapdir = os.getcwd()+'/snapshots'
 os.chdir(snapdir)
 
+# Nagrywanie streamów
+
+# os.system('ffmpeg -hide_banner -y -loglevel error -rtsp_transport tcp -use_wallclock_as_timestamps 1 -i rtsp://localhost:8554/a -vcodec copy -acodec copy -f segment -reset_timestamps 1 -segment_time 900 -segment_format mkv -segment_atclocktime 1 -strftime 1 test.mkv')
 
 # pętla snapshotowa
+
 for i in range(5):
     time.sleep(1)
     os.system(f'ffmpeg -skip_frame nokey -y -i {streamy_rtsp[0]} -vframes 1 test{i}.jpg')
@@ -31,4 +53,4 @@ for i in range(5):
 #
 #ffmpeg -re -i vid1.mp4 -f rtsp -rtsp_transport tcp  rtsp://localhost:8554/a
 #mediamtx
-#
+#r
